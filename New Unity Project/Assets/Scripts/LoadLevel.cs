aa0decb76d57d845537a854325fd16b03f88a04c
mgtsn <morgantosini@gmail.com>
2019-01-23 13:28:08 -0500

    Menu UI
    I added the beginnings of a menu and file select UI

---
+++ New Unity Project/Assets/Scripts/LoadLevel.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LoadLevel : MonoBehaviour {
+
+    
+
+    public void Load(int scene)
+    {
+        UnityEngine.SceneManagement.SceneManager.LoadScene(scene);
+    }
+
+    public void Quit()
+    {
+        Application.Quit();
+    }
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
