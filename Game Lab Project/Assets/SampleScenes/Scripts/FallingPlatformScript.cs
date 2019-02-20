daf42cdb452207a8fc81ea4f83ac8e16a332da0a
m <morgantosini@gmail.com>
2019-02-19 23:02:14 -0500

    Waves+logs physics

---
+++ Game Lab Project/Assets/SampleScenes/Scripts/FallingPlatformScript.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FallingPlatformScript : MonoBehaviour
+{
+    public Vector3 startPosition;
+
+    public float fallSpeed;
+    public float delay;
+    public float respawnTime;
+
+    private bool falling = false;
+
+    private float respawnCounter = 0;
+
+    void Respawn()
+    {
+        falling = false;
+        respawnCounter = 0;
+        transform.position = startPosition;
+    }
+
+    void OnCollisionEnter2D(Collision2D c)
+    {
+        if (c.collider.CompareTag("Player"))
+        {
+            StartCoroutine(Fall());
+        }
+    }
+
+    IEnumerator Fall()
+    {
+        yield return new WaitForSeconds(delay);
+        falling = true;
+        yield return 0;
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        startPosition = transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+       if (falling)
+        {
+            transform.Translate(0, -1 * fallSpeed * Time.deltaTime, 0);
+            respawnCounter += Time.deltaTime;
+            if (respawnCounter >= respawnTime)
+            {
+                Respawn();
+            }
+        }
+    }
+}
---
+++ Game Lab Project/Assets/SampleScenes/Scripts/Log.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Log : MonoBehaviour
+{
+    public GameObject ob;
+    public Mesh mesh;
+    private Vector3[] vertices;
+    public int i;
+
+    public float test = 0;
+    // Start is called before the first frame update
+    void Start()
+    {
+        mesh = ob.GetComponent<MeshFilter>().mesh;
+        vertices = mesh.vertices;
+        transform.position = vertices[i];  
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        test = vertices[i].x;
+        transform.position = Vector3.MoveTowards(transform.position, vertices[i], 1000);
+    }
+}
