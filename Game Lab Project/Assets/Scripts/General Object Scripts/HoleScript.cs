705ea89098780bd299abe5ed8cd345bfd40a8f6b
Josh Holmes <jlholmes.2016@mymail.becker.edu>
2019-04-23 14:53:38 -0400

    Preps for Recording

---
+++ Game Lab Project/Assets/Scripts/General Object Scripts/HoleScript.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HoleScript : MonoBehaviour {
+
+    GameObject player;
+    public GameObject linkedHole;
+
+	void Start () {
+        player = GameObject.Find("Player Controller");	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        player.transform.position = new Vector2(((linkedHole.transform.position.x) + 2), linkedHole.transform.position.y);
+
+    }
+}
