bb85adfa24eb5286ca7e65960da9d78fb25bd4a6
Evan McGrath <34551781+Tougon@users.noreply.github.com>
2019-07-23 17:05:52 -0400

    Initial Committ
    Integrated Sheets->Unity package. Did some tests. Setup's a pain, but it's otherwise pretty cool.

---
+++ Game Lab Project/Assets/Google Sheets to Unity/GetMeData.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using GoogleSheetsToUnity;
+
+public class GetMeData : MonoBehaviour {
+
+    private const string SHEET_ID = "13q-FhnZO64FzCASsVsMZ-OZtVz0IUmiS5QGSaNt9B1s";
+    private const string SHEET_NAME = "Sheet1";
+
+    // Use this for initialization
+    void Start () {
+
+        SpreadsheetManager.Read(new GSTU_Search(SHEET_ID, SHEET_NAME), DaTigaIsInDaCage);
+
+	}
+
+
+    void DaTigaIsInDaCage(GstuSpreadSheet spreadsheetRef)
+    {
+        Debug.Log(spreadsheetRef["C3"].value);
+
+        List<string> list1 = new List<string>()
+        {
+            "A",
+            "B",
+            "",
+            "D"
+        };
+
+        List<string> list2 = new List<string>()
+        {
+            "",
+            "2",
+            "3"
+        };
+
+        List<List<string>> values = new List<List<string>>() { list1, list2 };
+
+        SpreadsheetManager.Append(new GSTU_Search(SHEET_ID, SHEET_NAME), new ValueRange(values), null);
+
+        // What should happen is you should check if value exists. If it doesn't, append a row with all values initialized.
+    }
+}
